664cc9f5f4160818d81ddff45ef097d95021c5b4
prashantkbhardwaj <pkpbhardwaj729@gmail.com>
2019-10-30 14:40:22 +0530

    added recipe model

--- app/app/settings.py
+++ app/app/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql', 
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
